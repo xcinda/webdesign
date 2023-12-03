909a146eeb77e4ef034c046abe390aea983a9347
xcinda <xcinda@gmail.com>
2023-12-03 23:28:06 +0100

    menu inverse

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function tlacitkoklik(bary) {
     var elementy = document.getElementsByClassName("menu");
     bary.classList.toggle("krizek");
     Array.from(elementy).forEach((x) => {
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
+        if (x.style.display === "block") {
             x.style.display = "none";
+        } else {
+            x.style.display = "block";
         }
     })
 }
